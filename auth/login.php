1dc99019f03a9fee728dfab8718f960758f703f6
Pedro Lucidonio Berton Marques <plbertonm@gmail.com>
2026-09-22 09:25:46 -0300

    refactor(UX): Adicionados elementos faltantes a página de login

--- auth/login.php
+++ auth/login.php
@@ -6,9 +6,13 @@
     <title>Login</title>
 </head>
 <body>
+    <h1>Login - Churrasco</h1>
     <form action="autenticar.php" method="post">
-        <input type="text" name="usuario" id="usuario">
-        <input type="password" name="senha" id="senha">
+        <label for="usuario">Usuário:</label>
+        <input type="text" name="usuario" id="usuario" placeholder="Usuario...">
+        <label for="usuario">Senha:</label>
+        <input type="password" name="senha" id="senha" placeholder="Senha...">
+        <button type="submit">Logar</button>
     </form>
 </body>
 </html>
