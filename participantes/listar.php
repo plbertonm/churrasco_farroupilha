d54ed0a052797e4aa9fa9ce813e1220d1fd239c9
Marlon Damaceno da Silva <marlonds1110@gmail.com>
2026-09-22 11:20:40 -0300

    feat: Adicionado filtro de pagamento e presença na lista de participantes

--- participantes/listar.php
+++ participantes/listar.php
@@ -1,10 +1,24 @@
 <?php
 
 require_once "../config/conexao.php";
+
 $pesquisa = $_GET["pesquisa"] ?? "";
-$sql = "SELECT * FROM participantes WHERE Nome LIKE '%$pesquisa%'";
+$pagamento = $_GET["pagamento"] ?? "";
+$presenca = $_GET["presenca"] ?? "";
+
+$sql = "SELECT * FROM participantes WHERE nome LIKE '%$pesquisa%'";
+
+if ($pagamento !== "") {
+    $sql .= " AND pago = $pagamento";
+}
+
+if ($presenca !== "") {
+    $sql .= " AND confirmado = $presenca";
+}
+
 $resultado = $conexao->query($sql);
 ?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -15,8 +29,26 @@ $resultado = $conexao->query($sql);
 <h1>Lista de participantes</h1>
 
     <form action="#" method="get">
+
+    <h3>Pesquisar</h3>
         <input type="text" name="pesquisa" placeholder="Pesquisar participante" >
+
+    <h3>Pagamento</h3>
+    <select name="pagamento">
+        <option value="">Todos</option>
+        <option value="1">Pagos</option>
+        <option value="0">Pendentes</option>
+    </select>
+
+    <h3>Presenca</h3>
+    <select name="presenca">
+        <option value="">Todos</option>
+        <option value="1">Confirmados</option>
+        <option value="0">Não confirmados</option>
+    </select>
+
         <button type="submit">Pesquisar</button>
+
     </form>
 
 <table border="1">
@@ -33,8 +65,8 @@ $resultado = $conexao->query($sql);
             <td><?= htmlspecialchars($churras["nome"]) ?></td>
             <td><?= htmlspecialchars($churras["turma"]) ?></td>
             <td><?= htmlspecialchars($churras["tipo_churrasco"]) ?></td>
-            <td><?= htmlspecialchars($churras["confirmado"]) ?></td>
-            <td><?= htmlspecialchars($churras["pago"]) ?></td>
+            <td><?= ($churras["confirmado"] ? "Sim" : "Não") ?></td>
+            <td><?= ($churras["pago"] ? "Sim" : "Não") ?></td>
             <td>
                 <a href="editar.php?id=<?= $churras["id"] ?>">Editar</a>
                 <a href="excluir.php?id=<?= $churras["id"] ?>">Excluir</a>
