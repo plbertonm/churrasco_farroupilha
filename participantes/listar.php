f734d07b895e1561c1c90ba8c70f2d37dd871cfd
Marlon Damaceno da Silva <marlonds1110@gmail.com>
2026-09-22 11:05:22 -0300

    feat: Criada pesquisa na lista de participantes
    Mudado o nome dos dados enviados com post

--- participantes/listar.php
+++ participantes/listar.php
@@ -1,7 +1,8 @@
 <?php
 
 require_once "../config/conexao.php";
-$sql = "SELECT * FROM participantes";
+$pesquisa = $_GET["pesquisa"] ?? "";
+$sql = "SELECT * FROM participantes WHERE Nome LIKE '%$pesquisa%'";
 $resultado = $conexao->query($sql);
 ?>
 <!DOCTYPE html>
@@ -12,6 +13,12 @@ $resultado = $conexao->query($sql);
 </head>
 <body>
 <h1>Lista de participantes</h1>
+
+    <form action="#" method="get">
+        <input type="text" name="pesquisa" placeholder="Pesquisar participante" >
+        <button type="submit">Pesquisar</button>
+    </form>
+
 <table border="1">
     <tr>
         <th>Nome</th>
@@ -23,11 +30,11 @@ $resultado = $conexao->query($sql);
     </tr>
     <?php while ($churras = $resultado->fetch_assoc()): ?>
         <tr>
-            <td><?= htmlspecialchars($churras["Nome"]) ?></td>
-            <td><?= htmlspecialchars($churras["Turma"]) ?></td>
-            <td><?= htmlspecialchars($churras["Tipo"]) ?></td>
-            <td><?= htmlspecialchars($churras["Presenca"]) ?></td>
-            <td><?= htmlspecialchars($churras["Pagamento"]) ?></td>
+            <td><?= htmlspecialchars($churras["nome"]) ?></td>
+            <td><?= htmlspecialchars($churras["turma"]) ?></td>
+            <td><?= htmlspecialchars($churras["tipo_churrasco"]) ?></td>
+            <td><?= htmlspecialchars($churras["confirmado"]) ?></td>
+            <td><?= htmlspecialchars($churras["pago"]) ?></td>
             <td>
                 <a href="editar.php?id=<?= $churras["id"] ?>">Editar</a>
                 <a href="excluir.php?id=<?= $churras["id"] ?>">Excluir</a>
